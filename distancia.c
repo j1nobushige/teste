8b76e28b3ad54c7d84f6af4474181d725c72542e
j1 <nobushigejun@gmail.com>
2023-10-10 10:34:01 -0300

    Definicao das variaveis

--- distancia.c
+++ distancia.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-//funÁ„o que calcula a distancia
+//fun√ß√£o que calcula a distancia
 float calculaDistancia (int x1, int y1, int x2, int y2) {
-	return sqrt ( pow (x2 - x1, 2) + pow (y2 - y1, 2));
+	return sqrt (pow (x2 - x1, 2) + pow (y2 - y1, 2));
 }
 
-//funÁ„o principal
+//fun√ß√£o principal
 int main () {
+
+    //defini√ß√£o das vari√°veis
+    int x1, y1, x2, y2;
+    float distancia;
 }
